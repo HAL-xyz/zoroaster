603fa5f27c122f2ce96522c1bb5df2d9ddfc37e8
manlio <manlio.poltronieri@gmail.com>
2019-04-12 17:48:31 +0100

    Decode input data

---
+++ triggers/decodeinput.go
@@ -0,0 +1,75 @@
+package trigger
+
+import (
+	"encoding/hex"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"log"
+	"strings"
+)
+
+func DecodeInputData(data string, tsABI string) map[string]interface{} {
+
+	// load contract ABI
+	abi, err := abi.JSON(strings.NewReader(tsABI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+	decode method signature
+	strip hex prefix (0x)
+	signature is the first 32 bits of the has of the function
+	in HEX 1 char = 4 bits, so 32 bits = 8 chars
+	*/
+	decodedSig, err := hex.DecodeString(data[2:10])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// recover Method from signature and ABI
+	method, err := abi.MethodById(decodedSig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// decode function arguments
+	decodedData, err := hex.DecodeString(data[10:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	getMap := map[string]interface{}{}
+
+	// unpack method inputs
+	err = method.Inputs.UnpackIntoMap(getMap, decodedData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return getMap
+
+	// Unpack into struct:
+	//
+	//type Whatever struct {
+	//	TradeValues [8]*big.Int
+	//	TradeAddresses [4]common.Address
+	//	V [2]uint8
+	//	Rs [4][32]uint8
+	//}
+	//
+	//var what Whatever
+	//err = method.Inputs.Unpack(&what, decodedData)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	/*
+	Decoding Solidity => Go
+
+	uint256[8] => [8]*big.Int
+	addresses[4] => [4]common.Address
+	uint8[2] => [2]uint8
+	bytes32[4] => [4][32]uint8 (array of 4 arrays of 32 uint8 each)
+	*/
+
+}
