0bb432478aebc622043268ff4c93f2cd0c840bbd
manlio <manlio.poltronieri@gmail.com>
2019-05-16 13:05:12 +0100

    Use testify

--- triggers/decodeinput_test.go
+++ triggers/decodeinput_test.go
@@ -2,6 +2,7 @@ package trigger
 
 import (
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
 	"math/big"
 	"testing"
 )
@@ -12,33 +13,23 @@ func TestDecodeInputData(t *testing.T) {
 	const abi = `[{"constant":false,"inputs":[{"name":"assertion","type":"bool"}],"name":"assert","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"setOwner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"},{"name":"user","type":"address"},{"name":"nonce","type":"uint256"},{"name":"v","type":"uint8"},{"name":"r","type":"bytes32"},{"name":"s","type":"bytes32"},{"name":"feeWithdrawal","type":"uint256"}],"name":"adminWithdraw","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"lastActiveTransaction","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"}],"name":"depositToken","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"withdrawn","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"admins","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"admin","type":"address"},{"name":"isAdmin","type":"bool"}],"name":"setAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"tokens","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeAccount","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"invalidOrder","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"getOwner","outputs":[{"name":"out","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"user","type":"address"},{"name":"nonce","type":"uint256"}],"name":"invalidateOrdersBefore","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"traded","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"expiry","type":"uint256"}],"name":"setInactivityReleasePeriod","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"tradeValues","type":"uint256[8]"},{"name":"tradeAddresses","type":"address[4]"},{"name":"v","type":"uint8[2]"},{"name":"rs","type":"bytes32[4]"}],"name":"trade","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"inactivityReleasePeriod","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"token","type":"address"},{"name":"amount","type":"uint256"}],"name":"withdraw","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"orderFills","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"token","type":"address"},{"name":"user","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"feeAccount_","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"SetOwner","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenBuy","type":"address"},{"indexed":false,"name":"amountBuy","type":"uint256"},{"indexed":false,"name":"tokenSell","type":"address"},{"indexed":false,"name":"amountSell","type":"uint256"},{"indexed":false,"name":"expires","type":"uint256"},{"indexed":false,"name":"nonce","type":"uint256"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"v","type":"uint8"},{"indexed":false,"name":"r","type":"bytes32"},{"indexed":false,"name":"s","type":"bytes32"}],"name":"Order","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenBuy","type":"address"},{"indexed":false,"name":"amountBuy","type":"uint256"},{"indexed":false,"name":"tokenSell","type":"address"},{"indexed":false,"name":"amountSell","type":"uint256"},{"indexed":false,"name":"expires","type":"uint256"},{"indexed":false,"name":"nonce","type":"uint256"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"v","type":"uint8"},{"indexed":false,"name":"r","type":"bytes32"},{"indexed":false,"name":"s","type":"bytes32"}],"name":"Cancel","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"tokenBuy","type":"address"},{"indexed":false,"name":"amountBuy","type":"uint256"},{"indexed":false,"name":"tokenSell","type":"address"},{"indexed":false,"name":"amountSell","type":"uint256"},{"indexed":false,"name":"get","type":"address"},{"indexed":false,"name":"give","type":"address"}],"name":"Trade","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"token","type":"address"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"token","type":"address"},{"indexed":false,"name":"user","type":"address"},{"indexed":false,"name":"amount","type":"uint256"},{"indexed":false,"name":"balance","type":"uint256"}],"name":"Withdraw","type":"event"}]`
 
 	res, err := DecodeInputData(data, abi)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	tradeValues := res["tradeValues"].([8]*big.Int)
 
 	x := new(big.Int)
 	x.SetString("130500409274193550000000", 10)
 
-	if tradeValues[1].Cmp(x) != 0 {
-		t.Error()
-	}
+	assert.Equal(t, tradeValues[1].Cmp(x), 0)
 
 	tradeAddresses := res["tradeAddresses"].([4]common.Address)
-	if tradeAddresses[1].String() != "0x0198f46f520F33cd4329bd4bE380a25a90536CD5" {
-		t.Error()
-	}
+	assert.Equal(t, tradeAddresses[1].String(), "0x0198f46f520F33cd4329bd4bE380a25a90536CD5")
 
 	v := res["v"].([2]uint8)
 	y := [2]uint8{27, 28}
-	if v != y {
-		t.Error()
-	}
+	assert.Equal(t, v, y)
 
 	rs := res["rs"].([4][32]uint8) // bytes32[4]
 	z := [32]uint8{17, 86, 155, 208, 117, 140, 245, 163, 37, 39, 143, 234, 117, 232, 29, 175, 32, 117, 107, 199, 49, 109, 115, 115, 47, 27, 196, 125, 193, 219, 160, 90}
-	if rs[0] != z {
-		t.Error()
-	}
+	assert.Equal(t, rs[0], z)
 }
--- triggers/matching_test.go
+++ triggers/matching_test.go
@@ -1,272 +1,156 @@
 package trigger
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-// Testing one Filter VS one Transaction, Basic Filters
 func TestValidateFilter(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t1.json")
 
 	// BasicFilter / To
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error("Basic Filter / To should match")
-	}
-	if ValidateFilter(&block.Transactions[1], &trigger.Filters[0], &trigger.ContractABI) != false {
-		t.Error("Basic Filter / To should NOT match")
-	}
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[1], &trigger.Filters[0], &trigger.ContractABI), false)
 
 	// BasicFilter / Nonce
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error("Basic Filter / Nonce should match")
-	}
-	if ValidateFilter(&block.Transactions[4], &trigger.Filters[2], &trigger.ContractABI) != false {
-		t.Error("Basic Filter / Nonce should match")
-	}
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[2], &trigger.ContractABI), true)
 }
 
-// Testing one Filter VS one Transaction, Function Params
 func TestValidateFilter2(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t1.json")
 
-	// FunctionParameter / Address
-
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error("FuncParam should match")
-	}
-
-	if ValidateFilter(&block.Transactions[1], &trigger.Filters[1], &trigger.ContractABI) != false {
-		t.Error("FuncParam should NOT match")
-	}
+	// Address
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[1], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[1], &trigger.Filters[1], &trigger.ContractABI), false)
 }
 
-// BasicFilter / From
 func TestValidateFilter3(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t3.json")
 
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(&block.Transactions[5], &trigger.Filters[0], &trigger.ContractABI) != false {
-		t.Error()
-	}
+	// From
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[5], &trigger.Filters[0], &trigger.ContractABI), false)
 }
 
-// BasicFilter / Value, Gas, GasPrice
 func TestValidateFilter4(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t4.json")
 
 	// Value
-	if ValidateFilter(&block.Transactions[2], &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI) != false {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(&block.Transactions[2], &trigger.Filters[0], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[0], &trigger.ContractABI), false)
 
 	// Gas
-	if ValidateFilter(&block.Transactions[0], &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(&block.Transactions[5], &trigger.Filters[1], &trigger.ContractABI) != false {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(&block.Transactions[0], &trigger.Filters[1], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[5], &trigger.Filters[1], &trigger.ContractABI), false)
 
 	// GasPrice
-	if ValidateFilter(&block.Transactions[7], &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(&block.Transactions[4], &trigger.Filters[2], &trigger.ContractABI) != false {
-		t.Error()
-	}
-
+	assert.Equal(t, ValidateFilter(&block.Transactions[7], &trigger.Filters[2], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(&block.Transactions[4], &trigger.Filters[2], &trigger.ContractABI), false)
 }
 
-// Function Params, uint256[], bytes1...32[]
 func TestValidateFilter5(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t5.json")
 
 	// uint256[]
-	if ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI) != false {
-		t.Error()
-	}
-
-	if ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI) != false {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI), false)
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI), false)
 
 	// bytes14[]
-	if ValidateFilter(tx, &trigger.Filters[3], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(tx, &trigger.Filters[4], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[3], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[4], &trigger.ContractABI), true)
 }
 
 func TestValidateFilter6(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx2.json")
 	trigger := getTriggerFromFile("../resources/triggers/t6.json")
 
 	// address[N]
-	if ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI), true)
 
 	// uint256[N]
-	if ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI), true)
 }
 
 func TestValidateFilter7(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx3.json")
 	trigger := getTriggerFromFile("../resources/triggers/t7.json")
 
 	// uint256
-	if ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI), true)
 
 	// bool
-	if ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI), true)
 
 	// int128
-	if ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI), true)
 }
 
 func TestValidateFilter8(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx4.json")
 	trigger := getTriggerFromFile("../resources/triggers/t8.json")
 
 	// int128[N]
-	if ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI), true)
 
 	// int[N]
-	if ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI), true)
 
 	// int40
-	if ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI), true)
 }
 
 func TestValidateFilter9(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx5.json")
 	trigger := getTriggerFromFile("../resources/triggers/t9.json")
 
 	// int32
-	if ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[0], &trigger.ContractABI), true)
 
 	// int32[]
-	if ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[1], &trigger.ContractABI), true)
 
 	// int32[6]
-	if ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[2], &trigger.ContractABI), true)
 }
 
 func TestValidateFilter10(t *testing.T) {
-
 	tx := getTransactionFromFile("../resources/transactions/tx6.json")
 	trigger := getTriggerFromFile("../resources/triggers/t9.json")
 
 	// address[]
-	if ValidateFilter(tx, &trigger.Filters[3], &trigger.ContractABI) != true {
-		t.Error()
-	}
-
-	if ValidateFilter(tx, &trigger.Filters[4], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[3], &trigger.ContractABI), true)
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[4], &trigger.ContractABI), true)
 
 	// bytes1[]
-	if ValidateFilter(tx, &trigger.Filters[5], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[5], &trigger.ContractABI), true)
 
 	// string[]
-	if ValidateFilter(tx, &trigger.Filters[6], &trigger.ContractABI) != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateFilter(tx, &trigger.Filters[6], &trigger.ContractABI), true)
 }
 
 // Testing one Trigger vs one Transaction
 func TestValidateTrigger(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t1.json")
 
-	ok := ValidateTrigger(trigger, &block.Transactions[0])
-	if ok != true {
-		t.Error()
-	}
-
-	ok2 := ValidateTrigger(trigger, &block.Transactions[1])
-	if ok2 != false {
-		t.Error()
-	}
+	assert.Equal(t, ValidateTrigger(trigger, &block.Transactions[0]), true)
+	assert.Equal(t, ValidateTrigger(trigger, &block.Transactions[1]), false)
 }
 
 func TestValidateTrigger2(t *testing.T) {
-
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t2.json")
 
-	ok := ValidateTrigger(trigger, &block.Transactions[6])
-	if ok != true {
-		t.Error()
-	}
-
-	ok2 := ValidateTrigger(trigger, &block.Transactions[1])
-	if ok2 != false {
-		t.Error()
-	}
-
-	ok3 := ValidateTrigger(trigger, &block.Transactions[8])
-	if ok3 != true {
-		t.Error()
-	}
+	assert.Equal(t, ValidateTrigger(trigger, &block.Transactions[6]), true)
+	assert.Equal(t, ValidateTrigger(trigger, &block.Transactions[1]), false)
+	assert.Equal(t, ValidateTrigger(trigger, &block.Transactions[8]), true)
 }
 
 // Testing one Trigger vs one Block
@@ -274,8 +158,5 @@ func TestMatchTrigger(t *testing.T) {
 	block := getBlockFromFile("../resources/blocks/block1.json")
 	trigger := getTriggerFromFile("../resources/triggers/t2.json")
 
-	matches := MatchTrigger(trigger, block)
-	if matches != 2 {
-		t.Error()
-	}
+	assert.Equal(t, MatchTrigger(trigger, block), 2)
 }
