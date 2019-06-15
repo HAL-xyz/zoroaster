c3456e66f105f5e725ba4128b6009ffdaa44c402
manlio <manlio.poltronieri@gmail.com>
2019-06-15 17:34:52 +0200

    Fix integration test (wait for goroutine to finish)

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -3,19 +3,20 @@ package tests
 import (
 	"encoding/json"
 	"github.com/onrik/ethrpc"
+	log "github.com/sirupsen/logrus"
 	"io/ioutil"
-	"log"
 	"testing"
 	trig "zoroaster/triggers"
 )
 
 type AllRules struct {
-	Rules []struct {
-		Assert      string `json:"assert"`
-		Occurrences int    `json:"occurrences"`
-		BlockNo     int    `json:"block_no"`
-		TriggerFile string `json:"trigger_file"`
-	} `json:"rules"`
+	Rules []Rule
+}
+type Rule struct {
+	Assert      string `json:"assert"`
+	Occurrences int    `json:"occurrences"`
+	BlockNo     int    `json:"block_no"`
+	TriggerFile string `json:"trigger_file"`
 }
 
 func TestIntegration(t *testing.T) {
@@ -33,22 +34,26 @@ func TestIntegration(t *testing.T) {
 		log.Fatal(err)
 	}
 
+	doneChan := make(chan bool)
 	for _, r := range allRules.Rules {
-		go func() {
-			trigger, err := trig.NewTriggerFromFile("triggers/" + r.TriggerFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			block, err := client.EthGetBlockByNumber(r.BlockNo, true)
-			if err != nil {
-				log.Fatal(err)
-			}
-			txs := trig.MatchTrigger(trigger, block)
-
-			if len(txs) != r.Occurrences {
-				t.Error(r.Assert)
-			}
-		}()
+		go Run(doneChan, client, r, t)
 	}
+	<-doneChan
+}
 
+func Run(done chan bool, client *ethrpc.EthRPC, r Rule, t *testing.T) {
+	trigger, err := trig.NewTriggerFromFile("triggers/" + r.TriggerFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, err := client.EthGetBlockByNumber(r.BlockNo, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	txs := trig.MatchTrigger(trigger, block)
+
+	if len(txs) != r.Occurrences {
+		t.Error(r.Assert)
+	}
+	done <- true
 }
