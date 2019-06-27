ade5198e79a3c6ff447efef2dd14620f3fbc7b74
manlio <manlio.poltronieri@gmail.com>
2019-06-27 14:57:39 +0100

    Email templating for function params

--- actions/email.go
+++ actions/email.go
@@ -7,6 +7,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ses"
 	log "github.com/sirupsen/logrus"
+	"regexp"
+	"strconv"
 	"strings"
 	"zoroaster/triggers"
 )
@@ -79,16 +81,22 @@ func FillEmailTemplate(template string, ztx *trigger.ZTransaction) string {
 	body := template
 
 	// standard fields
-	body = strings.ReplaceAll(body, "$BlockNumber", string(*ztx.Tx.BlockNumber))
-	body = strings.ReplaceAll(body, "$BlockHash", ztx.Tx.BlockHash)
-	body = strings.ReplaceAll(body, "$TransactionHash", ztx.Tx.Hash)
-	body = strings.ReplaceAll(body, "$BlockTimestamp", string(ztx.BlockTimestamp))
-	body = strings.ReplaceAll(body, "$From", ztx.Tx.From)
-	body = strings.ReplaceAll(body, "$To", ztx.Tx.To)
-	body = strings.ReplaceAll(body, "$Value", ztx.Tx.Value.String())
-	body = strings.ReplaceAll(body, "$Gas", string(ztx.Tx.Gas))
-	body = strings.ReplaceAll(body, "$GasPrice", ztx.Tx.GasPrice.String())
-	body = strings.ReplaceAll(body, "$Nonce", string(ztx.Tx.Nonce))
+	blockNumber := fmt.Sprintf("%v", *ztx.Tx.BlockNumber)
+	blockTimestamp := fmt.Sprintf("%v", ztx.BlockTimestamp)
+	gas := fmt.Sprintf("%v", ztx.Tx.Gas)
+	gasPrice := fmt.Sprintf("%v", &ztx.Tx.GasPrice)
+	nonce := fmt.Sprintf("%v", ztx.Tx.Nonce)
+
+	body = strings.ReplaceAll(body, "$BlockNumber$", blockNumber)
+	body = strings.ReplaceAll(body, "$BlockHash$", ztx.Tx.BlockHash)
+	body = strings.ReplaceAll(body, "$TransactionHash$", ztx.Tx.Hash)
+	body = strings.ReplaceAll(body, "$BlockTimestamp$", blockTimestamp)
+	body = strings.ReplaceAll(body, "$From$", ztx.Tx.From)
+	body = strings.ReplaceAll(body, "$To$", ztx.Tx.To)
+	body = strings.ReplaceAll(body, "$Value$", ztx.Tx.Value.String())
+	body = strings.ReplaceAll(body, "$Gas$", gas)
+	body = strings.ReplaceAll(body, "$GasPrice$", gasPrice)
+	body = strings.ReplaceAll(body, "$Nonce$", nonce)
 
 	// function name
 	if ztx.DecodedFnName != nil {
@@ -97,17 +105,53 @@ func FillEmailTemplate(template string, ztx *trigger.ZTransaction) string {
 
 	// function params
 	if ztx.DecodedFnArgs != nil {
-		var args map[string]interface{}
+		var args map[string]json.RawMessage
 		err := json.Unmarshal([]byte(*ztx.DecodedFnArgs), &args)
 		if err != nil {
 			log.Error(err)
 			return body
 		}
-		for key, value := range args {
+
+		// replace !functionParams
+		for key, rawJson := range args {
 			old := fmt.Sprintf("!%s", key)
-			new := fmt.Sprintf("%v", value)
+			new := fmt.Sprintf("%s", rawJson)
 			body = strings.ReplaceAll(body, old, new)
+		}
+
+		// replace a function parameter with its indexed value, e.g. given
+		// ["0x0df721639ca2f7ff0e1f618b918a65ffb199ac4e",...][0] we want
+		// "0x0df721639ca2f7ff0e1f618b918a65ffb199ac4e"
+
+		indexedRgx := regexp.MustCompile(`\[\S*]\[\d*]`)
+		indexedParams := indexedRgx.FindAllString(body, -1)
+
+		arrayRgx := regexp.MustCompile(`]\[\d*]`)
+		for _, param := range indexedParams {
+			array := arrayRgx.FindString(param)   // matches ...][N]
+			array = removeCharacters(array, "[]") // N
+			index, err := strconv.Atoi(array)
+			if err != nil {
+				return body
+			}
+			splitElements := strings.Split(param, ",")
+			for i, e := range splitElements {
+				splitElements[i] = removeCharacters(e, "[]")
+			}
+			if index < len(splitElements) {
+				body = strings.Replace(body, param, splitElements[index], 1)
+			}
 		}
 	}
 	return body
+}
+
+func removeCharacters(input string, characters string) string {
+	filter := func(r rune) rune {
+		if strings.IndexRune(characters, r) < 0 {
+			return r
+		}
+		return -1
+	}
+	return strings.Map(filter, input)
 }
