081f6c74d0fb5b464a6b2b30aa3e5ce815400e50
manlio <manlio.poltronieri@gmail.com>
2020-05-22 16:18:10 +0100

    Add is_deleted to triggers table; tidy ups

--- db/migrate.sh
+++ db/migrate.sh
@@ -3,12 +3,17 @@
 if [[ -z "$1" ]]
   then
     echo "Usage: $0 <up|down>"
+    echo "To run N migration(s) up or down, pass \"up N\" as input"
     echo "You need to export the right local variables. See source"
     exit
 fi
 
 POSTGRESQL_URL="postgres://${DB_USR}:${DB_PWD}@${DB_URI}/${DB_NAME}?sslmode=disable"
 
-echo "Using Postgres URL: ${POSTGRESQL_URL}"
+read -p "You are going to migrate the db ${DB_NAME}; continue? [y\n] " CHOICE
+if [[ "$CHOICE" != "y" ]]; then
+    echo "Aborting"
+    exit
+fi
 
 migrate -database ${POSTGRESQL_URL} -path migrations $1
