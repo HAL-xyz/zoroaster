128388875af5eefad06ee86a12c3e0caeaf21416
manlio <manlio.poltronieri@gmail.com>
2020-06-29 11:41:14 +0100

    Remove old deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-echo 'Running tets...'
-./run_tests.sh
-echo 'Building Zoroaster...'
-env GOOS=linux GOARCH=amd64 go build -o zoroaster
-echo 'Uploading binary to ec2...'
-scp zoroaster ec2-user@ec2-3-125-138-199.eu-central-1.compute.amazonaws.com:.
-echo 'Done'
